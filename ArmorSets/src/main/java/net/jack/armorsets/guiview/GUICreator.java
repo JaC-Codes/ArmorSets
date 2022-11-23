0575596016f29b0edb25bcb426e1fd5acf58671b
Jack <118064473+JaC-Codes@users.noreply.github.com>
2022-11-23 17:28:20 +0000

    Update GUICreator.java

--- ArmorSets/src/main/java/net/jack/armorsets/guiview/GUICreator.java
+++ ArmorSets/src/main/java/net/jack/armorsets/guiview/GUICreator.java
@@ -1,20 +1,14 @@
 package net.jack.armorsets.guiview;
 
 import net.jack.armorsets.ArmorSets;
-import net.jack.armorsets.itembuilder.ItemBuild;
 import net.jack.armorsets.utils.CC;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -23,6 +17,7 @@ public class GUICreator {
 
     private final Inventory armorSetsGui;
     private final Inventory demonInspect;
+    private final Inventory warriorInspect;
 
     private final ArmorSets armorSets;
 
@@ -32,6 +27,8 @@ public class GUICreator {
                 , CC.translate(this.armorSets.getConfig().getString("ArmorSets.inventory.title")));
         demonInspect = Bukkit.createInventory(null, this.armorSets.getConfig().getInt("DemonSet.inventory.size")
                 , CC.translate(this.armorSets.getConfig().getString("DemonSet.inventory.title")));
+        warriorInspect = Bukkit.createInventory(null, this.armorSets.getConfig().getInt("WarriorSet.inventory.size")
+                , CC.translate(this.armorSets.getConfig().getString("WarriorSet.inventory.title")));
 
     }
 
@@ -63,12 +60,43 @@ public class GUICreator {
             for (final String l : this.armorSets.getConfig().getStringList("DemonSet.inventory.items." + i + ".lore")) {
                 lore.add(CC.translate(l));
             }
+            for (String enchant : this.armorSets.getConfig().getStringList("DemonSet.inventory.items." + i + ".enchants")) {
+                String enchantName = enchant.split(":")[0];
+                int enchantLevel = Integer.parseInt(enchant.split(":")[1]);
+                meta.addEnchant(Objects.requireNonNull(Enchantment.getByName(enchantName)), enchantLevel, true);
+            }
+
             meta.setLore(lore);
             item.setItemMeta(meta);
             demonInspect.setItem(this.armorSets.getConfig().getInt("DemonSet.inventory.items." + i + ".slot"), item);
         }
 
         player.openInventory(demonInspect);
+
+    }
+
+    public void openWarriorInspect(Player player) {
+        for (final String i : Objects.requireNonNull(this.armorSets.getConfig().getConfigurationSection("WarriorSet.inventory.items")).getKeys(false)) {
+            ItemStack item = new ItemStack(Material.valueOf(this.armorSets.getConfig().getString("WarriorSet.inventory.items." + i + ".item")));
+            ItemMeta meta = item.getItemMeta();
+            assert meta != null;
+            meta.setDisplayName(CC.translate(this.armorSets.getConfig().getString("WarriorSet.inventory.items." + i + ".name")));
+            ArrayList<String> lore = new ArrayList<>();
+            for (final String l : this.armorSets.getConfig().getStringList("WarriorSet.inventory.items." + i + ".lore")) {
+                lore.add(CC.translate(l));
+            }
+            for (String enchant : this.armorSets.getConfig().getStringList("WarriorSet.inventory.items." + i + ".enchants")) {
+                String enchantName = enchant.split(":")[0];
+                int enchantLevel = Integer.parseInt(enchant.split(":")[1]);
+                meta.addEnchant(Objects.requireNonNull(Enchantment.getByName(enchantName)), enchantLevel, true);
+            }
+
+            meta.setLore(lore);
+            item.setItemMeta(meta);
+            warriorInspect.setItem(this.armorSets.getConfig().getInt("WarriorSet.inventory.items." + i + ".slot"), item);
+        }
+
+        player.openInventory(warriorInspect);
 
     }
 
