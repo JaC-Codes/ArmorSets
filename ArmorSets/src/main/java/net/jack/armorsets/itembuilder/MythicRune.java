cb9a30cb78c2b4ecaa28d0b5d6d746e56d485e05
Jack <118064473+JaC-Codes@users.noreply.github.com>
2022-12-08 18:07:40 +0000

    Update MythicRune.java

--- ArmorSets/src/main/java/net/jack/armorsets/itembuilder/MythicRune.java
+++ ArmorSets/src/main/java/net/jack/armorsets/itembuilder/MythicRune.java
@@ -1,14 +1,15 @@
 package net.jack.armorsets.itembuilder;
 
 import net.jack.armorsets.ArmorSets;
+import net.jack.armorsets.timer.Timer;
 import net.jack.armorsets.utils.CC;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -19,15 +20,23 @@ import java.util.Objects;
 
 public class MythicRune implements Listener {
 
-    static NamespacedKey key = new NamespacedKey(ArmorSets.getInstance(), "mythicrune");
 
-    public static ItemStack getMythicRune() {
+    private final ArmorSets armorSets;
+    private final NamespacedKey key;
+    private final Timer timer;
+
+    public MythicRune(ArmorSets armorSets) {
+        this.armorSets = armorSets;
+        this.key = new NamespacedKey(armorSets, "mythicrune");
+        this.timer = new Timer(armorSets);
+    }
+
+    public ItemStack getMythicRune() {
         ItemStack mythicrune = new ItemBuild(Material.NETHER_STAR, 1)
                 .setDisplayName(CC.translate("&f&l** &r&d&lMythic Rune &f&l**"))
                 .setLore(CC.translate(" "), CC.translate("&d&oMythical rune"), CC.translate("&fx2 your xp intake for 2 hours once you redeem!"))
                 .build();
 
-        PersistentDataContainer pdc = Objects.requireNonNull(mythicrune.getItemMeta()).getPersistentDataContainer();
 
         ItemMeta meta = mythicrune.getItemMeta();
         meta.getPersistentDataContainer().set(key, PersistentDataType.STRING, "mythicrune");
@@ -52,9 +61,25 @@ public class MythicRune implements Listener {
             if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
                 if (meta == null) return;
                 if (meta.getPersistentDataContainer().has(key, PersistentDataType.STRING)) {
-                    item.setAmount(0);
+                    if (item.getAmount() >= 1) {
+                        item.setAmount(item.getAmount() - 1);
+                    }
+                   player.sendTitle(CC.translate("&a&lArmorEvents"), CC.translate("&b&lMythic Rune &a&lActivated"),
+                         30, 150, 30);
+                    timer.timerStart(player);
+                    this.armorSets.getTimers().setDoublexp(player.getUniqueId(), System.currentTimeMillis() + (this.armorSets.getConfig().getInt("Timers.double_exp.time")) * 1000L);
+
                 }
             }
+        }
+    }
+
+    @EventHandler
+    public void doubleExpEvent(PlayerExpChangeEvent event) {
+        Player player = event.getPlayer();
+        if (this.armorSets.getTimers().checkDoublexp(player.getUniqueId())) {
+            event.setAmount(event.getAmount() * 2);
+            player.sendMessage("Test");
         }
     }
 }
